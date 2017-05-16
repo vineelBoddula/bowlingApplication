ea3bfc2ae4b71913f7c3e371b9a84b4904ee2489
VinnyBodd <vineelbodd.01@gmail.com>
2017-05-15 23:39:02 -0400

    adding files

---
+++ src/bowlingapp/BowlingApp.java
@@ -0,0 +1,18 @@
+
+package bowlingapp;
+
+/**
+ *
+ * @author Vineel
+ */
+public class BowlingApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame j2 = new NewJFrame(); 
+        j2.setVisible(true); //Opens the NewJFrame
+    }
+
+}
